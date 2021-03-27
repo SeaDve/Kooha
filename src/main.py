b5dbec6688d23b4ca1dcf84cc30410f6300b724f
SeaDve <davecruz48@gmail.com>
2021-03-27 09:21:36 +0800

    Kooha: follow pep8

--- src/main.py
+++ src/main.py
@@ -19,8 +19,8 @@ import os
 import sys
 import gi
 
-gi.require_version('Gtk', '3.0')
 gi.require_version('Gst', '1.0')
+gi.require_version('Gtk', '3.0')
 gi.require_version('Handy', '1')
 
 from gettext import gettext as _
