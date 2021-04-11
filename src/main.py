dedbcaab294b47712992b7e90084f3911ad8add1
mathiascode <mail@mathias.is>
2021-04-11 22:03:02 +0300

    Avoid unnecessary calls to translation file when fetching translation

--- src/main.py
+++ src/main.py
@@ -16,7 +16,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
-from gettext import gettext as _
 
 import gi
 gi.require_version('Gst', '1.0')
--- src/window.py
+++ src/window.py
@@ -16,7 +16,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-from gettext import gettext as _
 from threading import Thread
 from time import localtime, strftime
 
