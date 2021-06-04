3e74993cfff26352eef131db83f782f3fbcde836
SeaDve <davecruz48@gmail.com>
2021-06-04 08:22:44 +0800

    refactor: Make timer more readable

--- src/backend/timer.py
+++ src/backend/timer.py
@@ -12,32 +12,28 @@ class Timer(GObject.GObject):
     __gsignals__ = {'delay-done': (GObject.SignalFlags.RUN_FIRST, None, ())}
 
     time = GObject.Property(type=int)
-    state = GObject.Property(type=int)
+    state = GObject.Property(type=int, default=TimerState.STOPPED)
 
     def __init__(self):
         super().__init__()
-        self.state = TimerState.STOPPED
         GLib.timeout_add_seconds(1, self._refresh_time, priority=GLib.PRIORITY_LOW)
 
     def _refresh_time(self):
         if self.state == TimerState.STOPPED:
             return True
-
         if self.time == 0:
-            self.emit('delay-done')
             self.state = TimerState.RUNNING
+            self.emit('delay-done')
         self.time += -1 if self.state == TimerState.DELAYED else 1
-
         return True
 
     def start(self, delay):
         if not delay:
-            self.emit('delay-done')
             self.state = TimerState.RUNNING
+            self.emit('delay-done')
         else:
             self.state = TimerState.DELAYED
-        self.delay = delay
-        self.time = delay
+        self.time = self.delay = delay
 
     def pause(self):
         self.state = TimerState.STOPPED
