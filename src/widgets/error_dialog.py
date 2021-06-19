1eafd3e1a234bf5c6f73e1733c0667e07108179f
SeaDve <davecruz48@gmail.com>
2021-06-19 08:12:02 +0800

    style: Organize kwargs in error dialog

--- src/widgets/error_dialog.py
+++ src/widgets/error_dialog.py
@@ -8,8 +8,8 @@ class ErrorDialog(Gtk.MessageDialog):
 
     def __init__(self, parent, title, text):
         super().__init__(modal=True,
-                         transient_for=parent,
                          buttons=Gtk.ButtonsType.OK,
+                         transient_for=parent,
                          title=title,
                          text=text)
         self.connect('response', lambda *_: self.close())
