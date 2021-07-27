cf12380b688bb3b30146d2aa43e7998a88027217
SeaDve <davecruz48@gmail.com>
2021-07-27 15:36:25 +0800

    chore: Localize app name

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use application::Application;
 use config::{GETTEXT_PACKAGE, LOCALEDIR, RESOURCES_FILE};
 use gettextrs::LocaleCategory;
 use gtk::{gio, glib};
+use i18n::i18n;
 
 fn main() {
     pretty_env_logger::init();
@@ -18,7 +19,7 @@ fn main() {
     gettextrs::bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR).expect("Unable to bind the text domain");
     gettextrs::textdomain(GETTEXT_PACKAGE).expect("Unable to switch to the text domain");
 
-    glib::set_application_name("Kooha");
+    glib::set_application_name(&i18n("Kooha"));
 
     gst::init().expect("Unable to start gstreamer");
     gtk::init().expect("Unable to start GTK4");
