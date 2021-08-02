05136882200aabd209b5817691ba6516e57e8ede
SeaDve <davecruz48@gmail.com>
2021-08-02 09:10:14 +0800

    fix: Inefficient to_string

--- src/i18n.rs
+++ src/i18n.rs
@@ -8,7 +8,7 @@ fn freplace(input: String, args: &[&str]) -> String {
     let mut parts = input.split("{}");
     let mut output = parts.next().unwrap_or("").to_string();
     for (p, a) in parts.zip(args.iter()) {
-        output += &(a.to_string() + &p.to_string());
+        output += &((*a).to_string() + &p.to_string());
     }
     output
 }
@@ -19,7 +19,7 @@ fn kreplace(input: String, kwargs: &[(&str, &str)]) -> String {
     for (k, v) in kwargs {
         if let Ok(re) = Regex::new(&format!("\\{{{}\\}}", k)) {
             s = re
-                .replace_all(&s, |_: &Captures<'_>| v.to_string())
+                .replace_all(&s, |_: &Captures<'_>| (*v).to_string())
                 .to_string();
         }
     }
