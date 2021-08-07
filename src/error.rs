147a291f62320144875c098a504be6ca7fc5379f
SeaDve <davecruz48@gmail.com>
2021-08-07 11:32:15 +0800

    fix: Include missing element errors

--- src/error.rs
+++ src/error.rs
@@ -5,8 +5,8 @@ use gtk::glib;
 #[derive(Debug, Clone)]
 pub enum Error {
     Portal(String),
-    Recorder(glib::Error),
     Pipeline(glib::Error),
+    Recorder(glib::Error),
 }
 
 impl std::error::Error for Error {}
@@ -14,9 +14,9 @@ impl std::error::Error for Error {}
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Portal(e) => f.write_str(&gettext!("Make sure to check for the runtime dependencies and \"It Doesn't Work\" page in Kooha's Github page. ({})", e)),
+            Self::Portal(e) => f.write_str(&gettext!("Make sure to check for the runtime dependencies and \"It Doesn't Work\" page in Kooha's readme page. ({})", e)),
+            Self::Pipeline(e) => f.write_str(&gettext!("A gstreamer plugin may not be installed. If not, please report to Kooha's issue page. ({})", e)),
             Self::Recorder(e) => f.write_str(&format!("{}", e)),
-            Self::Pipeline(e) => f.write_str(&gettext!("Please report to the issue page. ({})", e)),
         }
     }
 }
