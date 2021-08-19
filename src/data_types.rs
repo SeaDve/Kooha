ba1f8e8972616367589584d2b80e408ae253b010
SeaDve <davecruz48@gmail.com>
2021-08-19 08:26:27 +0800

    chore: Add tests for rectangle

--- src/data_types.rs
+++ src/data_types.rs
@@ -64,3 +64,83 @@ impl Screen {
         Self { width, height }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use approx::{assert_relative_eq, assert_ulps_eq};
+
+    #[test]
+    fn rectangle_default() {
+        let p1 = Point::new(0.0, 0.0);
+        let p2 = Point::new(0.0, 0.0);
+        let rect = Rectangle::from_points(&p1, &p2);
+
+        assert_relative_eq!(rect.x, 0.0);
+        assert_relative_eq!(rect.y, 0.0);
+        assert_relative_eq!(rect.width, 0.0);
+        assert_relative_eq!(rect.height, 0.0);
+    }
+
+    #[test]
+    fn rectangle_smaller_p1() {
+        let p1 = Point::new(5.0, 6.0);
+        let p2 = Point::new(10.0, 13.0);
+        let rect = Rectangle::from_points(&p1, &p2);
+
+        assert_relative_eq!(rect.x, 5.0);
+        assert_relative_eq!(rect.y, 6.0);
+        assert_relative_eq!(rect.width, 5.0);
+        assert_relative_eq!(rect.height, 7.0);
+    }
+
+    #[test]
+    fn rectangle_smaller_p2() {
+        let p1 = Point::new(10.0, 13.0);
+        let p2 = Point::new(5.0, 6.0);
+        let rect = Rectangle::from_points(&p1, &p2);
+
+        assert_relative_eq!(rect.x, 5.0);
+        assert_relative_eq!(rect.y, 6.0);
+        assert_relative_eq!(rect.width, 5.0);
+        assert_relative_eq!(rect.height, 7.0);
+    }
+
+    #[test]
+    fn rectangle_mixed_smaller_1() {
+        let p1 = Point::new(10.0, 6.0);
+        let p2 = Point::new(5.0, 13.0);
+        let rect = Rectangle::from_points(&p1, &p2);
+
+        assert_relative_eq!(rect.x, 5.0);
+        assert_relative_eq!(rect.y, 6.0);
+        assert_relative_eq!(rect.width, 5.0);
+        assert_relative_eq!(rect.height, 7.0);
+    }
+
+    #[test]
+    fn rectangle_mixed_smaller_2() {
+        let p1 = Point::new(5.0, 10.0);
+        let p2 = Point::new(13.0, 6.0);
+        let rect = Rectangle::from_points(&p1, &p2);
+
+        assert_relative_eq!(rect.x, 5.0);
+        assert_relative_eq!(rect.y, 6.0);
+        assert_relative_eq!(rect.width, 8.0);
+        assert_relative_eq!(rect.height, 4.0);
+    }
+
+    #[test]
+    fn rectangle_rescale() {
+        let p1 = Point::new(8.1, 13.7);
+        let p2 = Point::new(14.3, 11.3);
+        let rect = Rectangle::from_points(&p1, &p2);
+        let rescaled_rect = rect.rescale(5.0);
+
+        assert_relative_eq!(rescaled_rect.x, 8.1 * 5.0);
+        assert_relative_eq!(rescaled_rect.y, 11.3 * 5.0);
+        assert_ulps_eq!(rescaled_rect.width, 6.2 * 5.0);
+        assert_ulps_eq!(rescaled_rect.height, 2.4 * 5.0);
+    }
+}
