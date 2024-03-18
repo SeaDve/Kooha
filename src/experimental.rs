ef102204257dc4e95b768de0364be3a3be76a39e
Dave Patrick Caberto <davecruz48@gmail.com>
2024-03-18 14:15:25 +0800

    refactor(experimental): use OnceCell over Lazy

--- src/experimental.rs
+++ src/experimental.rs
@@ -1,26 +1,24 @@
-use std::env;
-
-use once_cell::sync::Lazy;
-
-static ENABLED_FEATURES: Lazy<Vec<Feature>> = Lazy::new(|| {
-    env::var("KOOHA_EXPERIMENTAL")
-        .map(|val| {
-            val.split(',')
-                .filter_map(|raw_feature_str| {
-                    let feature_str = raw_feature_str.trim().to_lowercase();
-                    let feature = Feature::from_str(&feature_str);
-                    if feature.is_none() {
-                        tracing::warn!("Unknown `{}` experimental feature", feature_str);
-                    }
-                    feature
-                })
-                .collect::<Vec<_>>()
-        })
-        .unwrap_or_default()
-});
+use std::{env, sync::OnceLock};
 
 pub fn enabled_features() -> &'static [Feature] {
-    ENABLED_FEATURES.as_ref()
+    static ENABLED_FEATURES: OnceLock<Vec<Feature>> = OnceLock::new();
+
+    ENABLED_FEATURES.get_or_init(|| {
+        env::var("KOOHA_EXPERIMENTAL")
+            .map(|val| {
+                val.split(',')
+                    .filter_map(|raw_feature_str| {
+                        let feature_str = raw_feature_str.trim().to_lowercase();
+                        let feature = Feature::from_str(&feature_str);
+                        if feature.is_none() {
+                            tracing::warn!("Unknown `{}` experimental feature", feature_str);
+                        }
+                        feature
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .unwrap_or_default()
+    })
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -43,6 +41,8 @@ impl Feature {
     }
 
     pub fn is_enabled(self) -> bool {
-        ENABLED_FEATURES.contains(&Self::All) || ENABLED_FEATURES.contains(&self)
+        let enabled_features = enabled_features();
+
+        enabled_features.contains(&Self::All) || enabled_features.contains(&self)
     }
 }
