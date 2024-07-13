6a32993345845bc4e0aee9e901d1ee6c71fb2ced
Dave Patrick Caberto <davecruz48@gmail.com>
2024-07-13 08:46:48 +0800

    build(aux): update dist-vendor from gtk-rust-template

--- build-aux/dist-vendor.sh
+++ build-aux/dist-vendor.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/bin/sh
+# Since Meson invokes this script as
+# "/bin/sh .../dist-vendor.sh DIST SOURCE_ROOT" we can't rely on bash features
+set -eu
 export DIST="$1"
 export SOURCE_ROOT="$2"
 
 cd "$SOURCE_ROOT"
 mkdir "$DIST"/.cargo
-cargo vendor | sed 's/^directory = ".*"/directory = "vendor"/g' > $DIST/.cargo/config
+# cargo-vendor-filterer can be found at https://github.com/coreos/cargo-vendor-filterer
+# It is also part of the Rust SDK extension.
+cargo vendor-filterer --platform=x86_64-unknown-linux-gnu --platform=aarch64-unknown-linux-gnu > "$DIST"/.cargo/config
+rm -f vendor/gettext-sys/gettext-*.tar.*
+# Don't combine the previous and this line with a pipe because we can't catch
+# errors with "set -o pipefail"
+sed -i 's/^directory = ".*"/directory = "vendor"/g' "$DIST/.cargo/config"
 # Move vendor into dist tarball directory
 mv vendor "$DIST"
 
