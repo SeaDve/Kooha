97ff1a75efa2d7eb1e9746d12077ea2df50af9a9
Dave Patrick Caberto <davecruz48@gmail.com>
2025-08-23 12:13:10 +0800

    build(dist-vendor): update based on gtk-rust-template

--- build-aux/dist-vendor.sh
+++ build-aux/dist-vendor.sh
@@ -9,12 +9,18 @@ cd "$SOURCE_ROOT"
 mkdir "$DIST"/.cargo
 # cargo-vendor-filterer can be found at https://github.com/coreos/cargo-vendor-filterer
 # It is also part of the Rust SDK extension.
-cargo vendor-filterer --platform=x86_64-unknown-linux-gnu --platform=aarch64-unknown-linux-gnu > "$DIST"/.cargo/config
-rm -f vendor/gettext-sys/gettext-*.tar.*
+cargo vendor-filterer --all-features --platform=x86_64-unknown-linux-gnu --platform=aarch64-unknown-linux-gnu > "$DIST"/.cargo/config.toml
+set -- vendor/gettext-sys/gettext-*.tar.*
+TARBALL_PATH=$1
+TARBALL_NAME=$(basename "$TARBALL_PATH")
+rm -f "$TARBALL_PATH"
 # remove the tarball from checksums
-echo $(jq -c 'del(.files["gettext-0.21.tar.xz"])' vendor/gettext-sys/.cargo-checksum.json) > vendor/gettext-sys/.cargo-checksum.json
+cargo_checksum='vendor/gettext-sys/.cargo-checksum.json'
+tmp_f=$(mktemp --tmpdir='vendor/gettext-sys' -t)
+jq -c "del(.files[\"$TARBALL_NAME\"])" "$cargo_checksum" > "$tmp_f"
+mv -f "$tmp_f" "$cargo_checksum"
 # Don't combine the previous and this line with a pipe because we can't catch
 # errors with "set -o pipefail"
-sed -i 's/^directory = ".*"/directory = "vendor"/g' "$DIST/.cargo/config"
+sed -i 's/^directory = ".*"/directory = "vendor"/g' "$DIST/.cargo/config.toml"
 # Move vendor into dist tarball directory
 mv vendor "$DIST"
